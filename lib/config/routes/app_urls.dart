1d88d5597d2c4721499c814e6ed19f14be567be9
muhammedrafeeqkk <rafeeqshan06@gmail.com>
2025-01-03 10:09:29 +0400

    app urls updated

--- lib/config/routes/app_urls.dart
+++ lib/config/routes/app_urls.dart
@@ -1,7 +1,7 @@
 class AppUrls {
   // static String loginAPI = "https://reqres.in/api/login";
-  static String loginAPIs = "http://35.154.68.50/api/customer/auth/login";
-    static String loginAPI = "http://35.154.68.50/api/consultant/auth/login";
+  static String loginAPI = "http://35.154.68.50/api/customer/auth/login";
+   // static String loginAPI = "http://35.154.68.50/api/consultant/auth/login";
 
 }
 
